cdb9b4a57d6411d9959b04bfb66497f5ee494725
federicocrovetti <federico.crovetti@studio.unibo.it>
2022-01-17 17:44:25 +0100

    Create prediction.py
    Module to perform predictions on new data

---
+++ prediction.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+#calling the trained network for predictions
+
+import tensorflow as tf
+import numpy as np
+from pathlib import Path
+import argparse
+from argparse import RawTextHelpFormatter
+from dataload import DataLoad
+from prediction_feeder import PredictionDataFeeder
+from stackandsplit import NormDict, StackedData
+from IPython.display import display
+
+
+def display_mask(i):
+    mask = np.argmax(val_preds[i], axis=-1)
+    mask = np.expand_dims(mask, axis=-1)
+    img = tf.keras.preprocessing.image.array_to_img(mask)
+    display(img)
+
+
+
+if __name__ == '__main__':
+    
+    patients = []
+    data = []
+    masks = []
+    data_folders = []
+    
+    parser = argparse.ArgumentParser(description = 
+                                     '''Module to access the trained network and to make evaluation on new data.
+                                          
+                                         '''
+                                         , formatter_class=RawTextHelpFormatter)
+    parser.add_argument('basepath', 
+                        type = str, 
+                        help='Path to the working directory in which the data is contained')
+    
+    parser.add_argument('network', 
+                        type = str, 
+                        help='Path to the file which contains the trained network')
+    
+    parser.add_argument('img_size', 
+                        type = tuple, 
+                        help='Tuple containing the sizes of the images (sizes must be equal along X and Y axes). Sizes must match with those the nework was trained with')
+
+    parser.add_argument('batch_size', 
+                        type = int, 
+                        help='Batch for the loading of the images to be evalued')    
+    
+    
+    args = parser.parse_args()
+    
+    if args.basepath:
+        basepath = Path(args.basepath) 
+        
+
+    files_in_basepath = basepath.iterdir()
+    for item in files_in_basepath:
+        if item.is_dir():
+            if not item.name == '__pycache__' and not item.name == '.hypothesis':
+                print(item.name)
+                data_folders.append(item.name)
+                path = basepath / '{}'.format(item.name)
+                patients.append(path)
+                
+    files_in_basepath = basepath.iterdir()
+    for item in files_in_basepath:
+        if item.is_dir():
+            if not item.name == '__pycache__' and not item.name == '.hypothesis':
+                for elem in item.iterdir():
+                    if "Data" in elem.name:
+                        data.append(elem)
+                    elif "Segmentation" in elem.name:
+                        masks.append(elem)
+    
+    data, data_array = DataLoad(data, masks)
+    del(data)
+    data = StackedData(data_array)
+    del(data_array)
+    
+    model = tf.keras.models.load_model('{}'.format(args.network))
+    
+    test_gen = PredictionDataFeeder(args.batch_size, args.img_size, data['features'])
+    val_preds = model.predict(test_gen)
+    
+    for i in range(len(data['features'])):
+        display_mask(i)
+        img = data['features'][i]
+        img = np.expand_dims(img, axis = -1)
+        img = tf.keras.preprocessing.image.array_to_img(img)
+        display(img)
