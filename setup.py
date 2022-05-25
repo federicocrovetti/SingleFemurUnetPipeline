86f06760350d3605820e1f4b2bc61803bcdda6ba
federico_crovetti <48324746+federicocrovetti@users.noreply.github.com>
2022-05-25 11:56:47 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,94 @@
+import os
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+
+except ImportError:
+  from distutils.core import setup
+  from distutils.core import find_packages
+
+def get_requires (requirements_filename):
+  '''
+  What packages are required for this module to be executed?
+  Parameters
+  ----------
+    requirements_filename : str
+      filename of requirements (e.g requirements.txt)
+  Returns
+  -------
+    requirements : list
+      list of required packages
+  '''
+  with open(requirements_filename, 'r') as fp:
+    requirements = fp.read()
+
+  return list(filter(lambda x: x != '', requirements.split()))
+
+
+
+def read_description (readme_filename):
+  '''
+  Description package from filename
+  Parameters
+  ----------
+    readme_filename : str
+      filename with readme information (e.g README.md)
+  Returns
+  -------
+    description : str
+      str with description
+  '''
+
+  try:
+
+    with open(readme_filename, 'r') as fp:
+      description = '\n'
+      description += fp.read()
+
+    return description
+
+  except IOError:
+    return ''
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+#Package-Metadata
+NAME = "UnetSingleFemurSegmentation"
+DESCRIPTION = 'Package for Semantic Segmentation of CT images of femurs with a single label'
+URL = 'https://github.com/federicocrovetti/2DUnetFemurSegmentation'
+EMAIL = 'federico.crovetti@studio.unibo.it'
+AUTHOR = 'Federico Crovetti, Riccardo Biondi'
+VERSION = 1.0.0
+KEYWORDS = 'artificial-intelligence machine-learning deep-learning medical-imaging tensorflow u-net single-femur femur'
+REQUIREMENTS_FILENAME = os.path.join(here, 'requirements.txt')
+#VERSION_FILENAME = os.path.join(here, 'CTLungSeg', '__version__.py')
+README_FILENAME = os.path.join(here, 'README.md')
+
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    mantainer=AUTHOR,
+    mantainer_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=URL,
+    download_url=URL,
+    keywords=KEYWORDS,
+    packages=find_packages(include=['utils','model'], exclude=('tests')),
+    include_package_data=True, # no absolute paths are allowed
+    platforms='any',
+    install_requires=get_requires(REQUIREMENTS_FILENAME),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7.11',
+    license = 'MIT'
+)
