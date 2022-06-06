1d581d5d1bef1ad882292c1fd3a60fa1f205d449
federico_crovetti <48324746+federicocrovetti@users.noreply.github.com>
2022-06-06 17:49:49 +0200

    added reconstruction from ext metadata

--- SFUNet/utils/bonereconstruction.py
+++ SFUNet/utils/bonereconstruction.py
@@ -16,7 +16,7 @@ def Reconstruction(data, ID, boundingbox, new_folder_path, train = False):
     data : dict type object with 'features' and 'labels' as keys containins sitk.Image objects
     ID : string representing the sample's ID (patient's folder in the case of the resampling of a
          pre-existing image or the numerical ID with which to identify the folder of a new patient)
-    boundingbox : txt file containing the bounding boxes and the lower and upper coordinates on y-axis where the patch was extracted
+    boundingbox : list containing the bounding boxes and the lower and upper coordinates on y-axis where the patch was extracted
                     from the original image
     new_folder_path : the parent folder for the new data
 
@@ -41,6 +41,75 @@ def Reconstruction(data, ID, boundingbox, new_folder_path, train = False):
             join.SetGlobalDefaultCoordinateTolerance(14.21e-1)
             recon_volume = join.Execute([reconstructed['features'][k] for k in range(len(reconstructed['features']))])
             MDTransfer(data['features'][i], recon_volume)
+        
+            NIFTISingleSampleWriter(recon_volume, recon_labels, ID[i], new_folder_path)
+        
+    else:
+        for i in range(len(data['features'])):
+            print(data['features'][i].GetSize())
+        for i in range(len(data['features'])):
+            reconstructed = {'features' : [], 'labels' : []}
+            for j in range(len(boundingbox[i])):
+            
+                cut_feat = data['features'][i][:, :, j][:,:]
+                rec_slice_feat = sitk.Image(256, 512, sitk.sitkInt16)
+                MDTransfer(cut_feat, rec_slice_feat)
+                rec_slice_feat[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_feat #, y_min : y_max] = cut_feat
+                rec_slice_feat.SetOrigin([-9.1640584e+01, -1.8851562e+02])
+        
+                cut_lab = data['labels'][i][:, :, j][:,:]
+                rec_slice_lab = sitk.Image(256, 512, sitk.sitkInt16)
+                MDTransfer(cut_lab, rec_slice_lab)
+                rec_slice_lab[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_lab# y_min : y_max] = cut_lab
+                rec_slice_lab.SetOrigin([-9.1640584e+01, -1.8851562e+02])
+                reconstructed['features'].append(rec_slice_feat)
+                reconstructed['labels'].append(rec_slice_lab)
+            
+            join = sitk.JoinSeriesImageFilter()
+            join.SetGlobalDefaultCoordinateTolerance(14.21e-1)
+            recon_volume = join.Execute([reconstructed['features'][k] for k in range(len(reconstructed['features']))])
+            recon_labels = join.Execute([reconstructed['labels'][k] for k in range(len(reconstructed['labels']))])
+            MDTransfer(data['features'][i], recon_volume)
+            MDTransfer(data['labels'][i], recon_labels)
+        
+            NIFTISampleWriter(recon_volume, recon_labels, ID[i], new_folder_path)
+        
+    return
+
+def ReconstructionExtMetadata(data, ID, boundingbox, new_folder_path, md, train = False):
+    """
+    
+    Parameters
+    ----------
+    data : dict type object with 'features' and 'labels' as keys containins sitk.Image objects
+    ID : string representing the sample's ID (patient's folder in the case of the resampling of a
+         pre-existing image or the numerical ID with which to identify the folder of a new patient)
+    boundingbox : list containing the bounding boxes and the lower and upper coordinates on y-axis where the patch was extracted
+                    from the original image
+    new_folder_path : the parent folder for the new data
+    md : list containing the metadata of the original image
+
+
+    """
+    if train == False:
+        for i in range(len(data['features'])):
+            print(data['features'][i].GetSize())
+        for i in range(len(data['features'])):
+            reconstructed = {'features' : []}
+            for j in range(len(boundingbox[i])):
+            
+                cut_feat = data['features'][i][:, :, j][:,:]
+                rec_slice_feat = sitk.Image(256, 512, sitk.sitkInt16
+                rec_slice_feat[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_feat #, y_min : y_max] = cut_feat
+                rec_slice_feat.SetOrigin([-9.1640584e+01, -1.8851562e+02])
+       
+                reconstructed['features'].append(rec_slice_feat)
+            
+            join = sitk.JoinSeriesImageFilter()
+            join.SetGlobalDefaultCoordinateTolerance(14.21e-1)
+            recon_volume = join.Execute([reconstructed['features'][k] for k in range(len(reconstructed['features']))])
+            recon_volume.SetSpacing((md[0], md[1], md[2]))
+            recon_volume.SetOrigin((md[3], md[4], md[5]))
         
             NIFTISingleSampleWriter(recon_volume, recon_labels, ID[i], new_folder_path)
         
@@ -127,7 +196,12 @@ if __name__ == '__main__':
     parser.add_argument('csv_path', 
                         metavar='csv_path',
                         type = str, 
-                        help='True when we have labels (training phase), False when we do not. The default is True.')
+                        help='Path to the csv containing the bounding boxes')
+    
+    parser.add_argument('metadata_path', 
+                        metavar='metadata_path',
+                        type = str, 
+                        help='Path to the csv where each row contains the metadata of the original volume. ([spacing x y z origin x y x]) ')
     
     args = parser.parse_args() 
     
@@ -169,11 +243,17 @@ if __name__ == '__main__':
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             bounding_box_list.append(row)
+    
+    metadata_list = []
+    with open(Path('{}'.format(args.metadata_path)), 'r', newline='', encoding='UTF8') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            metadata_list.append(row)
  
     
     iterator = iter(bounding_box_list)
     bounding_box_grouped = [list(islice(iterator, elem))
           for elem in img_sizes]
                 
-    Reconstruction(dataset, ID, bounding_box_grouped, new_folder_path, train = args.train)
+    ReconstructionExtMetadata(dataset, ID, bounding_box_grouped, new_folder_path, metadata_list, train = args.train)
 
