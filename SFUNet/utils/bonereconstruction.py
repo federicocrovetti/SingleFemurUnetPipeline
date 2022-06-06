77102c55250f025f9a216d7d40bcea1c0c46b2a2
federico_crovetti <48324746+federicocrovetti@users.noreply.github.com>
2022-06-06 14:51:53 +0200

    added optional parameter 'train' for single or double writing output (features + labels)

--- SFUNet/utils/bonereconstruction.py
+++ SFUNet/utils/bonereconstruction.py
@@ -6,9 +6,9 @@ from itertools import islice
 import argparse
 from argparse import RawTextHelpFormatter
 from pathlib import Path
-from SFUNet.utils.dataload import DataLoad, NIFTISampleWriter, MDTransfer
+from SFUNet.utils.dataload import DataLoad, NIFTISampleWriter, NIFTISingleSampleWriter, MDTransfer
 
-def Reconstruction(data, ID, boundingbox, new_folder_path):
+def Reconstruction(data, ID, boundingbox, new_folder_path, train = False):
     """
     
     Parameters
@@ -22,39 +22,59 @@ def Reconstruction(data, ID, boundingbox, new_folder_path):
 
 
     """
-    for i in range(len(data['features'])):
-        print(data['features'][i].GetSize())
-    for i in range(len(data['features'])):
-        reconstructed = {'features' : [], 'labels' : []}
-        for j in range(len(boundingbox[i])):
+    if train == False:
+        for i in range(len(data['features'])):
+            print(data['features'][i].GetSize())
+        for i in range(len(data['features'])):
+            reconstructed = {'features' : []}
+            for j in range(len(boundingbox[i])):
             
-            cut_feat = data['features'][i][:, :, j][:,:]
-            rec_slice_feat = sitk.Image(256, 512, sitk.sitkInt16)
-            MDTransfer(cut_feat, rec_slice_feat)
-            rec_slice_feat[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_feat #, y_min : y_max] = cut_feat
-            rec_slice_feat.SetOrigin([-9.1640584e+01, -1.8851562e+02])
-        
-            cut_lab = data['labels'][i][:, :, j][:,:]
-            rec_slice_lab = sitk.Image(256, 512, sitk.sitkInt16)
-            MDTransfer(cut_lab, rec_slice_lab)
-            rec_slice_lab[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_lab# y_min : y_max] = cut_lab
-            rec_slice_lab.SetOrigin([-9.1640584e+01, -1.8851562e+02])
-            reconstructed['features'].append(rec_slice_feat)
-            reconstructed['labels'].append(rec_slice_lab)
+                cut_feat = data['features'][i][:, :, j][:,:]
+                rec_slice_feat = sitk.Image(256, 512, sitk.sitkInt16)
+                MDTransfer(cut_feat, rec_slice_feat)
+                rec_slice_feat[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_feat #, y_min : y_max] = cut_feat
+                rec_slice_feat.SetOrigin([-9.1640584e+01, -1.8851562e+02])
+       
+                reconstructed['features'].append(rec_slice_feat)
             
-        join = sitk.JoinSeriesImageFilter()
-        join.SetGlobalDefaultCoordinateTolerance(14.21e-1)
-        recon_volume = join.Execute([reconstructed['features'][k] for k in range(len(reconstructed['features']))])
-        recon_labels = join.Execute([reconstructed['labels'][k] for k in range(len(reconstructed['labels']))])
-        MDTransfer(data['features'][i], recon_volume)
-        MDTransfer(data['labels'][i], recon_labels)
+            join = sitk.JoinSeriesImageFilter()
+            join.SetGlobalDefaultCoordinateTolerance(14.21e-1)
+            recon_volume = join.Execute([reconstructed['features'][k] for k in range(len(reconstructed['features']))])
+            MDTransfer(data['features'][i], recon_volume)
         
-        NIFTISampleWriter(recon_volume, recon_labels, ID[i], new_folder_path)
+            NIFTISingleSampleWriter(recon_volume, recon_labels, ID[i], new_folder_path)
+        
+    else:
+        for i in range(len(data['features'])):
+            print(data['features'][i].GetSize())
+        for i in range(len(data['features'])):
+            reconstructed = {'features' : [], 'labels' : []}
+            for j in range(len(boundingbox[i])):
+            
+                cut_feat = data['features'][i][:, :, j][:,:]
+                rec_slice_feat = sitk.Image(256, 512, sitk.sitkInt16)
+                MDTransfer(cut_feat, rec_slice_feat)
+                rec_slice_feat[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_feat #, y_min : y_max] = cut_feat
+                rec_slice_feat.SetOrigin([-9.1640584e+01, -1.8851562e+02])
+        
+                cut_lab = data['labels'][i][:, :, j][:,:]
+                rec_slice_lab = sitk.Image(256, 512, sitk.sitkInt16)
+                MDTransfer(cut_lab, rec_slice_lab)
+                rec_slice_lab[0 : 256, int(boundingbox[i][j][4]) : int(boundingbox[i][j][5])] = cut_lab# y_min : y_max] = cut_lab
+                rec_slice_lab.SetOrigin([-9.1640584e+01, -1.8851562e+02])
+                reconstructed['features'].append(rec_slice_feat)
+                reconstructed['labels'].append(rec_slice_lab)
+            
+            join = sitk.JoinSeriesImageFilter()
+            join.SetGlobalDefaultCoordinateTolerance(14.21e-1)
+            recon_volume = join.Execute([reconstructed['features'][k] for k in range(len(reconstructed['features']))])
+            recon_labels = join.Execute([reconstructed['labels'][k] for k in range(len(reconstructed['labels']))])
+            MDTransfer(data['features'][i], recon_volume)
+            MDTransfer(data['labels'][i], recon_labels)
+        
+            NIFTISampleWriter(recon_volume, recon_labels, ID[i], new_folder_path)
         
     return
-
-
-
 
 
 if __name__ == '__main__':
@@ -101,7 +121,7 @@ if __name__ == '__main__':
     
     parser.add_argument('train', 
                         metavar='train',
-                        type = str, 
+                        type = bool, 
                         help='True when we have labels (training phase), False when we do not. The default is True.')
     
     parser.add_argument('csv_path', 
@@ -155,5 +175,5 @@ if __name__ == '__main__':
     bounding_box_grouped = [list(islice(iterator, elem))
           for elem in img_sizes]
                 
-    Reconstruction(dataset, ID, bounding_box_grouped, new_folder_path)
+    Reconstruction(dataset, ID, bounding_box_grouped, new_folder_path, train = args.train)
 
