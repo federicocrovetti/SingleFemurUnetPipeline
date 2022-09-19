2c3de6f62966021daef2bee468f7bf762a183fea
federicocrovetti <federico.crovetti@studio.unibo.it>
2022-09-19 10:30:36 +0200

    Added Path explorer and modified DataLoad for both batch and sequential loading

--- SFUNet/utils/dataload.py
+++ SFUNet/utils/dataload.py
@@ -75,76 +75,109 @@ def NrrdReader(path):
     return image, reader_nrrd
 
 
+def PathExplorer(basepath):
+    """
+    
+    Parameters
+    ----------
+    basepath : Pathlib Path to the parent directory containing the data
+
+    Returns
+    -------
+    patients : List of paths to the folders, containing data and masks, of each sample in the parent directory
+    data : List of paths to all of the images in each 'patients' folder
+    masks : List of paths to all of the masks in each 'patients' folder
+    data_folders : List of names of the folders in parent directory
+
+    """
+    patients = []
+    data = []
+    masks = []
+    data_folders = []
+    
+    files_in_basepath = basepath.iterdir()
+    for item in files_in_basepath:
+        if item.is_dir():
+            if not item.name == '__pycache__' and not item.name == '.hypothesis' and not item.name == '_logdir_' and not item.name == 'Fedz' and not item.name == '.pytest_cache':
+                print(item.name)
+                data_folders.append(item.name)
+                path = basepath / '{}'.format(item.name)
+                patients.append(path)
+                for elem in item.iterdir():
+                    if "Data" in elem.name:
+                        data.append(elem)
+                    elif "Segmentation" in elem.name:
+                        masks.append(elem)
+                
+    return patients, data, masks, data_folders 
+
 def DataLoad(data_path, masks_path):
     """
     Parameters
     ----------
-    data_path : list of WindowsPaths to the folders containing the images (DICOM Series, NIFTI, ...)
-    masks_path : list of WindowsPaths to the folders containing the labels (DICOM Series, NIFTI, ...)
+    data_path : list of Pathlib Paths to the folders containing the images (DICOM Series, NIFTI, ...)
+    masks_path : list of Pathlib Paths to the folders containing the labels (DICOM Series, NIFTI, ...)
     
     Returns
     -------
-    data_and_labels : a dict type object containing sitk.Image format images and segmentations
-    data_and_labels_array : a dict type object containing numpy format images and segmentations
+    data_and_labels : list of dict type object containing sitk.Image format images and segmentations
+    data_and_labels_array : list of dict type object containing numpy format images and segmentations
     
     
-    Batch load of the images in the selected paths. The preferred data formats to read are DICOM of NIFTI for
+    Can be used for both batch and sequential loading. The preferred data formats to read are DICOM of NIFTI for
     the features and NIFTI or DICOM or NRRD for the labels.
     """
+    
     data_and_labels = {'features': [], 'labels':[]}
     data_and_labels_array = {'features': [], 'labels':[]}
     
-    for path in data_path:
-        item_list = []
-        for item in path.iterdir():
-            item_list.append(item.name)
-        if any(".dcm" in i for i in item_list):
-            image , reader_dicom_data = DicomReader(path)
+    item_list = []
+    for item in data_path.iterdir():
+        item_list.append(item.name)
+    if any(".dcm" in i for i in item_list):
+        image , reader_dicom_data = DicomReader(data_path)
+        image_array = sitk.GetArrayFromImage(image)
+        image_array = np.transpose(image_array, axes=[1, 2, 0])
+        data_and_labels['features'].append(image)
+        data_and_labels_array['features'].append(image_array)
+        
+    else:
+        for item in data_path.iterdir():
+            item = str(item)
+            image, reader_data = NiftiReader(item)
             image_array = sitk.GetArrayFromImage(image)
             image_array = np.transpose(image_array, axes=[1, 2, 0])
             data_and_labels['features'].append(image)
             data_and_labels_array['features'].append(image_array)
+   
+    item_list = []
+    for item in masks_path.iterdir():
+        item_list.append(item.name)
+    if any(".dcm" in i for i in item_list):
+        image , reader_dicom_masks = DicomReader(masks_path)
+        image_array = sitk.GetArrayFromImage(image)
+        image_array = np.transpose(image_array, axes=[1, 2, 0])
+        data_and_labels['labels'].append(image)
+        data_and_labels_array['labels'].append(image_array)
+    
+                
+    elif any(".nrrd" in i for i in item_list):
+        for item in masks_path.iterdir():
+            item = str(item)
+            image, reader_masks = NrrdReader(item)
+            image_array = sitk.GetArrayFromImage(image)
+            image_array = np.transpose(image_array, axes=[1, 2, 0])
+            data_and_labels['labels'].append(image)
+            data_and_labels_array['labels'].append(image_array) 
             
-        else:
-            for item in path.iterdir():
-                item = str(item)
-                image, reader_data = NiftiReader(item)
-                image_array = sitk.GetArrayFromImage(image)
-                image_array = np.transpose(image_array, axes=[1, 2, 0])
-                data_and_labels['features'].append(image)
-                data_and_labels_array['features'].append(image_array)
-            
-            
-    for path in masks_path:
-        item_list = []
-        for item in path.iterdir():
-            item_list.append(item.name)
-        if any(".dcm" in i for i in item_list):
-            image , reader_dicom_masks = DicomReader(path)
+    else:
+        for item in masks_path.iterdir():
+            item = str(item)
+            image, reader_masks = NiftiReader(item)
             image_array = sitk.GetArrayFromImage(image)
             image_array = np.transpose(image_array, axes=[1, 2, 0])
             data_and_labels['labels'].append(image)
             data_and_labels_array['labels'].append(image_array)
-        
-                    
-        elif any(".nrrd" in i for i in item_list):
-            for item in path.iterdir():
-                item = str(item)
-                image, reader_masks = NrrdReader(item)
-                image_array = sitk.GetArrayFromImage(image)
-                image_array = np.transpose(image_array, axes=[1, 2, 0])
-                data_and_labels['labels'].append(image)
-                data_and_labels_array['labels'].append(image_array)                              
-                    
-        else:
-            for item in path.iterdir():
-                item = str(item)
-                image, reader_masks = NiftiReader(item)
-                image_array = sitk.GetArrayFromImage(image)
-                image_array = np.transpose(image_array, axes=[1, 2, 0])
-                data_and_labels['labels'].append(image)
-                data_and_labels_array['labels'].append(image_array)
-
             
     return data_and_labels, data_and_labels_array
 
@@ -246,81 +279,8 @@ def MDTransfer(in_image, out_image):
     out_image.SetDirection(direction)
     out_image.SetSpacing(spacing)
     
-    return 
+    return
 
 
 if __name__ == '__main__':
-    
-    patients = []
-    data = []
-    masks = []
-    data_folders = []
-    
-    parser = argparse.ArgumentParser(description = 
-                                     '''Module for the batch data loading and preprocessing. With optional argument --resample operates a reshaping (deprecated for new versions in favour of greylevel thresholding.
-                                     Take as input a variety of combination of data in the form of Dicom, Nifti and Nrrd.
-                                     Recomended structure for the directory containing the data:
-                                     
-                                     DATA  
-                                         |
-                                         Sample1
-                                             |
-                                             Images
-                                                 |
-                                                     >  ----
-                                             |
-                                             Labels
-                                                 |
-                                                     >  ----
-                                          |
-                                          Sample2
-                                          .
-                                          .
-                                          .
-                                          
-                                         '''
-                                         , formatter_class=RawTextHelpFormatter)
-    parser.add_argument('basepath', 
-                        type = str, 
-                        help='Path to the working directory in which the data is contained')
-    
-    parser.add_argument('-dl',
-                        '--dataloader', 
-                        type = str,
-                        help='Path to the new directory onto which the resampled images will be written')
-    
-    parser.add_argument('-res', 
-                        '--resample', 
-                        type = str,
-                        help='Path to the new directory onto which the resampled images will be written')
-    args = parser.parse_args()
-    
-    if args.basepath:
-        basepath = Path(args.basepath) 
-        
-
-    
-    
-    files_in_basepath = basepath.iterdir()
-    for item in files_in_basepath:
-        if item.is_dir():
-            if not item.name == '__pycache__' and not item.name == '.hypothesis':
-                print(item.name)
-                data_folders.append(item.name)
-                path = basepath / '{}'.format(item.name)
-                patients.append(path)
-                
-    files_in_basepath = basepath.iterdir()
-    for item in files_in_basepath:
-        if item.is_dir():
-            if not item.name == '__pycache__' and not item.name == '.hypothesis':
-                for elem in item.iterdir():
-                    if "Data" in elem.name:
-                        data.append(elem)
-                    elif "Segmentation" in elem.name:
-                        masks.append(elem)
-    
-    
-    if args.dataloader:
-        dataset, dataset_array = DataLoad(data, masks)
-
+    pass
