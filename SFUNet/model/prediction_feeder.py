68304843c25ff5fcc7c8debabbb2285d43bc712a
federicocrovetti <federico.crovetti@studio.unibo.it>
2022-10-14 15:07:21 +0200

    added docstring to PredictionFeeder

--- SFUNet/model/prediction_feeder.py
+++ SFUNet/model/prediction_feeder.py
@@ -3,7 +3,11 @@ import tensorflow as tf
 import numpy as np
 
 class PredictionFeeder(tf.keras.utils.Sequence):
-    """Helper to iterate over the data (as Numpy arrays)."""
+    """
+    Class for iteration over multiple source files. Data must be already loaded into memory;
+    After that is divied in a number of batches, accordingly to the batch size and image lenght 
+    along z axis, for them to be converted into numpy ndarrays to be fed to the network for prediction.
+    """
 
     def __init__(self, batch_size, img_size, features_dataset):
         self.batch_size = batch_size
