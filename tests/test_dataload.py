17992eef42c3e32526671df99a7948ef6c3397d5
federicocrovetti <federico.crovetti@studio.unibo.it>
2022-10-18 12:10:56 +0200

    added testing for image equality

--- tests/test_dataload.py
+++ tests/test_dataload.py
@@ -84,6 +84,8 @@ def test_DataLoad(datas, labelss, IDs, new_folder_pathss):
 
   for i in range(len(data_paths)):
     dataset, dataset_array = DataLoad(data_paths[i], masks_paths[i])
+    assert (dataset['features'][0] == data).all()
+    assert (dataset['labels'][0] == labels).all()
     assert (dataset['features'][0].GetSize() == data.GetSize())  
     assert (dataset['labels'][0].GetSize() == labels.GetSize())  
     assert (dataset['features'][0].GetDirection() == pytest.approx(data.GetDirection()))
